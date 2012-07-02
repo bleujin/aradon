fcc1e34d2c0720983fa005687885413b5745c09d
bleujin <bleujin@gmail.com>
2012-07-02 18:46:38 +0900

    ..

--- src/net/ion/nradon/netty/codec/http/websocketx/CloseWebSocketFrame.java
+++ src/net/ion/nradon/netty/codec/http/websocketx/CloseWebSocketFrame.java
@@ -15,35 +15,63 @@
  */
 package net.ion.nradon.netty.codec.http.websocketx;
 
+import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.util.CharsetUtil;
 
 /**
  * Web Socket Frame for closing the connection
  */
 public class CloseWebSocketFrame extends WebSocketFrame {
 
-    /**
-     * Creates a new empty close frame.
-     */
-    public CloseWebSocketFrame() {
-        setBinaryData(ChannelBuffers.EMPTY_BUFFER);
-    }
+	/**
+	 * Creates a new empty close frame.
+	 */
+	public CloseWebSocketFrame() {
+		setBinaryData(ChannelBuffers.EMPTY_BUFFER);
+	}
 
-    /**
-     * Creates a new close frame
-     * 
-     * @param finalFragment
-     *            flag indicating if this frame is the final fragment
-     * @param rsv
-     *            reserved bits used for protocol extensions
-     */
-    public CloseWebSocketFrame(boolean finalFragment, int rsv) {
-        setFinalFragment(finalFragment);
-        setRsv(rsv);
-    }
+	/**
+	 * Creates a new close frame
+	 * 
+	 * @param finalFragment
+	 *            flag indicating if this frame is the final fragment
+	 * @param rsv
+	 *            reserved bits used for protocol extensions
+	 */
+	public CloseWebSocketFrame(boolean finalFragment, int rsv) {
+		setFinalFragment(finalFragment);
+		setRsv(rsv);
+	}
 
-    @Override
-    public String toString() {
-        return getClass().getSimpleName();
-    }
+	public int getStatusCode() {
+		ChannelBuffer binaryData = this.getBinaryData();
+		if (binaryData == null || binaryData.capacity() == 0) {
+			return -1;
+		}
+
+		binaryData.readerIndex(0);
+		int statusCode = binaryData.readShort();
+		binaryData.readerIndex(0);
+
+		return statusCode;
+	}
+
+	public String getReasonText() {
+		ChannelBuffer binaryData = this.getBinaryData();
+		if (binaryData == null || binaryData.capacity() <= 2) {
+			return "";
+		}
+
+		binaryData.readerIndex(2);
+		String reasonText = binaryData.toString(CharsetUtil.UTF_8);
+		binaryData.readerIndex(0);
+
+		return reasonText;
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getSimpleName();
+	}
 }
